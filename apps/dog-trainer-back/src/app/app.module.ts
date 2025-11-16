e29e6d72d35a8c07e6a1f10bcb50ae3c3c7c2bd4
Simon Clr <simonclerc.pro@gmail.com>
2025-11-16 16:21:25 +0100

    refactor: structure back

--- apps/dog-trainer-back/src/app/app.module.ts
+++ apps/dog-trainer-back/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { SessionController } from './session.controller';
-import { SessionService } from './session.service';
+import { SessionController } from './session/session.controller';
+import { SessionService } from './session/session.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, SessionController],
-  providers: [AppService, SessionService],
+  controllers: [SessionController],
+  providers: [SessionService],
 })
 export class AppModule {}
