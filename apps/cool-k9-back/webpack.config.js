120bbbe86ffffd66e9599ee44121acd3298fa9e4
Simon Clr <simonclerc.pro@gmail.com>
2026-08-23 12:45:10 +0200

    fix: back output build path

--- apps/cool-k9-back/webpack.config.js
+++ apps/cool-k9-back/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/dog-trainer-back'),
+    path: join(__dirname, '../../dist/apps/cool-k9-back'),
     clean: true,
     ...(process.env.NODE_ENV !== 'production' && {
       devtoolModuleFilenameTemplate: '[absolute-resource-path]',
